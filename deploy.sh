3a64476b00c05a1331a8160c77dc5d16fdf8e880
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-12 15:20:35 +0000

    excelsior

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ sudo yum install nodejs20-npm -y
 sudo npm install -g npm@11.3.0
 sudo npm install -g pm2
 sudo npm install -g serve
-cd /home/ec2-user/
+cd /home/ec2-user/client
 
 # Detener procesos anteriores si existen
 pm2 delete all || true
