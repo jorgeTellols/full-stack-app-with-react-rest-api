a300132cf70e1be730ef59a0e987e68a54a51f49
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-12 14:05:35 +0000

    modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,6 @@ cd /home/ec2-user/app
 # Detener procesos anteriores si existen
 pm2 delete all || true
 
-# Backend
-cd api
-npm install
-npm run seed
-pm2 start npm --name backend -- start
-cd ..
-
 # Frontend
 cd frontend
 npm install -g serve
