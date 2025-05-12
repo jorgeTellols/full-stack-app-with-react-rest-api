333a7212ff3dee4362c2f9c7df28dd494eccd871
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-12 14:47:28 +0000

    Modified pipeline files

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-cd /home/ec2-user/app
+sudo yum install nodejs20-npm -y
+sudo npm install -g npm@11.3.0
+sudo npm install -g pm2
+sudo npm install -g serve
+cd /home/ec2-user/
 
 # Detener procesos anteriores si existen
 pm2 delete all || true
 
 # Frontend
 cd frontend
-npm install -g serve
-pm2 serve . 3000 --name frontend
+pm2 serve . 3000 --name ./
 
 pm2 save
 pm2 startup
