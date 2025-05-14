b7163e46308b0160ce8305b26dd6053619a0252d
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-14 16:07:30 +0000

    otra vez vvenga

--- afterInstall.sh
+++ afterInstall.sh
@@ -3,9 +3,9 @@ cd ~/build
 sudo pm2 serve . 80
 pm2 save
 sudo pm2 startup
-cd  ~/api
-sudo npm install
-npm run seed
-sudo pm2 serve . 5000
-pm2 save
-sudo pm2 startup
+# cd  ~/api
+# sudo npm install
+# npm run seed
+# sudo pm2 serve . 5000
+# pm2 save
+# sudo pm2 startup
