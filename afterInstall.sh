52e444c8e8faee8e2ecd7cdfc47ffa7cbc2d515e
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-15 09:54:36 +0000

    lo conseguí 100% confirmado

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ~/build
-sudo pm2 serve . 80
-sudo save
+sudo pm2 serve . 80 --name clientsudo save
 sudo pm2 startup
 cd  ~/api
 sudo npm install
 sudo npm run seed
+sudo pm2 start npm --name "api" -- run start
+sudo pm2 save 
+sudo pm2 startup
