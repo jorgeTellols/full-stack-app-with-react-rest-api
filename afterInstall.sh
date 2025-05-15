33ace31975f474703f3699e57ec159401e90c0ed
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-15 10:18:41 +0000

    Que pasa si no pueblo la bBDD?

--- afterInstall.sh
+++ afterInstall.sh
@@ -4,7 +4,7 @@ sudo pm2 serve . 80 --name clientsudo save
 sudo pm2 startup
 cd  ~/api
 sudo npm install
-sudo npm run seed
+# sudo npm run seed
 sudo pm2 start npm --name "api" -- run start
 sudo pm2 save 
 sudo pm2 startup
