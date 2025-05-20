44edce7e94c7176a958e14bd1b1977a7667367b1
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-20 11:44:15 +0000

    montalo tio

--- afterInstall.sh
+++ afterInstall.sh
@@ -5,9 +5,6 @@ sudo pm2 save
 sudo pm2 startup
 cd  ~/api
 sudo npm install
-sudo pm2 start npm --name api -- run start
+sudo pm2 start app.js --name api
 sudo pm2 save 
 sudo pm2 startup
-# sudo dnf update -y
-# sudo dnf install mariadb105 -y
-# mysql --version
