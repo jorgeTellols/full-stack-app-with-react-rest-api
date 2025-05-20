b873c3f53b4db81de356ded31512625eebd67aa9
EC2 Default User <ec2-user@ip-10-0-4-252.eu-south-2.compute.internal>
2025-05-20 09:32:43 +0000

    m,mmm

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 cd ~/build
-sudo pm2 serve . 80 --name clientsudo save
+sudo pm2 serve . 80 --name client
+sudo pm2 save
 sudo pm2 startup
 cd  ~/api
 sudo npm install
-# sudo npm run seed
-sudo pm2 start npm --name "api" -- run start
+sudo pm2 start npm --name api -- run start
 sudo pm2 save 
 sudo pm2 startup
-#!/bin/bash
-sudo dnf update -y
-sudo dnf install mariadb105 -y
-mysql --version
+# sudo dnf update -y
+# sudo dnf install mariadb105 -y
+# mysql --version
