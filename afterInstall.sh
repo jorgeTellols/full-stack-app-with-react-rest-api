c508d3a4be832f4d045ee7f7a3a58d54f0ccfb04
EC2 Default User <ec2-user@ip-10-2-6-121.eu-south-2.compute.internal>
2025-05-23 16:14:37 +0000

    switched to nginx

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd ~/build
-sudo pm2 serve . 80 --name client
-sudo pm2 save
-sudo pm2 startup
-cd  ~/api
-sudo npm install nodemon
+sudo mkdir -p /var/www/frontend
+sudo cp -r ~/build/* /var/www/frontend/
+sudo cp ~/proyecto-final.conf /etc/nginx/conf.d/proyecto-final.conf
+sudo systemctl enable nginx
+sudo systemctl restart nginx
+cd ~/api
+sudo npm install
 sudo pm2 start app.js --name api
 sudo pm2 save 
 sudo pm2 startup
--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install nodejs20-npm -y
+sudo yum update -y
+sudo yum install -y nginx nodejs20-npm
 sudo npm install -g npm@11.3.0
 sudo npm install -g pm2
-sudo npm install -g serve
 sudo pm2 delete all || true
