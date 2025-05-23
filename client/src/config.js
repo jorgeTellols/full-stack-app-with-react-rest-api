cc0e741f9e3522a76b8cdf0f49d7753a498c325d
EC2 Default User <ec2-user@ip-10-2-6-121.eu-south-2.compute.internal>
2025-05-23 18:44:20 +0000

    changed api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: `${window.location.protocol}//${window.location.hostname}:5000/api`,
+  apiBaseUrl: `${window.location.protocol}//${window.location.hostname}/api`,
 };
 
 export default config;
